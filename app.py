d7f23af6e447cc0ac4aeca4ab3fa1cbb6707f411
vipulkarke <82372312+vipulkarke@users.noreply.github.com>
2024-12-05 18:06:47 -0500

    update

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask
+import socket
 
 app = Flask(__name__)
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
-@app.route("/")
-def home():
-    return "Welcome to vipulkarke Final Test API Server"
+@app.route('/')
+def hello_cloud():
+  return 'Hello Cloud!'
+  
+@app.route('/host')
+def host_name():
+  return hostname
 
-@app.route("/api/health")
-def health():
-    return jsonify({"status": "healthy"}), 200
+@app.route('/ip')
+def host_ip():
+  return ip_address
 
-@app.route("/api/greet/<name>")
-def greet(name):
-    return jsonify({"message": f"Hello, {name}! Welcome to the API."}), 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+app.run(host='0.0.0.0')
